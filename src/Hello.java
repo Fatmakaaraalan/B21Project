a438fdd5c93560184d45a57a4124472b63a60e37
Fatmakaaraalan <fatmauk@hotmail.com>
2021-01-29 17:16:10 +0000

    COMMITING MORE CHANGES

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,10 @@ public class Hello {
         System.out.println("hello world");
         //more stuff
         //windows users are happy to see commit dialog finally
+        //i have already published on Github.com
+        //now i am ready to make commits
+        //but do i need to share again ??
+        //ABSULUTELLY NO
+        //IT'S TIME COMMIT AND JUST PUSH!!!!
     }
 }
