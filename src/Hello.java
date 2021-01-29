19e4c0326c8a404b6e62f613a542daecbea5ea34
Fatmakaaraalan <fatmauk@hotmail.com>
2021-01-29 14:23:23 +0000

    added new stuff

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        //more stuff
+        //windows users are happy to see commit dialog finally
     }
 }
